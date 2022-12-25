6b06ec7e5ba71eece6a84200484ac082e26eaab6
hkr0416 <hkr0416@gmail.com>
2022-12-25 15:20:49 +0900

    add this

--- baekjoon.js
+++ baekjoon.js
@@ -1,30 +1,24 @@
 // import fs from 'fs';
 const fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().trim(); //readFileSync(0) : stdin
+let input = fs.readFileSync('./input.txt').toString().trim().split('\n'); //readFileSync(0) : stdin
 
 // const readline = require('readline');
 // const rl = readline.createInterface(fs.createReadStream('./input.txt'));
+const range = new Array(Number(input[0])).fill(0).map((el, idx) => idx+1);
+const numbers = input.slice(1).map(Number);
 
-function counter() {
-    let value = 0;
-    return {
-        increase : function() {
-            value++;
-        }, 
-        decrease : function() {
-            value--;
-        }, 
-        seeValue : function() {
-            return value;
-        }
-    }
-}
+// const id = Symbol('id');
 
-const counter1 = counter();
-console.log(counter1.seeValue());
-counter1.increase();
-console.log(counter1.seeValue());
-counter1.increase();
-console.log(counter1.seeValue());
-counter1.decrease();
-console.log(counter1.seeValue());
+// const obj = {
+//     [id] : 1,
+//     name : 'lsh'
+// }
+
+// const obj1 = Object.assign({}, obj);
+// const obj2 = {...obj}
+
+// console.log(obj1);
+// console.log(obj2);
+// console.log(obj1['id'])
+// console.log(Object.keys(obj));
+
