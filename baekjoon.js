871fc67b6e1a6517ffcec2f0d712314971b07847
hkr0416 <hkr0416@gmail.com>
2023-01-02 08:39:56 +0900

    callstack

--- baekjoon.js
+++ baekjoon.js
@@ -5,26 +5,72 @@ let input = fs.readFileSync('./input.txt').toString().trim(); //readFileSync(0) 
 // const readline = require('readline');
 // const rl = readline.createInterface(fs.createReadStream('./input.txt'));
 
-function counter() {
-    let value = 0;
-    return {
-        increase : function() {
-            value++;
-        }, 
-        decrease : function() {
-            value--;
-        }, 
-        seeValue : function() {
-            return value;
-        }
-    }
+function seeThis (...arg) {
+    console.log(this, ...arg);
 }
 
-const counter1 = counter();
-console.log(counter1.seeValue());
-counter1.increase();
-console.log(counter1.seeValue());
-counter1.increase();
-console.log(counter1.seeValue());
-counter1.decrease();
-console.log(counter1.seeValue());
+const bindObj = {
+    name : 'bindObj'
+}
+
+const bindObj2 = {
+    name : 'bindObj2'
+}
+
+const bindSeeThis = seeThis.bind(bindObj, 1, 2, 3).bind(bindObj2, 4, 5);
+
+const obj1 = {
+    name : 'obj1',
+    // seeThis
+    bindSeeThis
+}
+
+const obj2 = {
+    name : 'obj2',
+    // seeThis
+    bindSeeThis
+}
+
+const arr = [() => { console.log(this)}];
+
+seeThis();
+// obj1.seeThis();
+// obj2.seeThis();
+
+bindSeeThis(10);
+obj1.bindSeeThis(11);
+obj2.bindSeeThis(12);
+arr[0]();
+
+// bindSeeThis.call({ name : `I'm new object`}, 13);
+
+// const objLiteral = {
+//     name : 'literal',
+//     seeThis : () => {
+//         console.log(this);
+//     }
+// }
+
+// class MakeObj {
+//     constructor(arg) {
+//         this.name = arg;
+//     }
+//     seeThis = () => {
+//         console.log(this);
+//     }
+// }
+
+// const objInstance = new MakeObj('class');
+
+// objLiteral.seeThis();
+// objInstance.seeThis();
+
+// const arr = new Array(8).fill(1);
+
+// arr.seeThis = function() {
+//     console.log(this);
+// }
+
+// arr.seeThis();
+
+
