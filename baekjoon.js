6a118254a275b2290ff66756e5a875ebf5cd6ffc
hkr0416 <hkr0416@gmail.com>
2023-02-13 20:32:18 +0900

    just

--- baekjoon.js
+++ baekjoon.js
@@ -1,88 +1,28 @@
 // import fs from 'fs';
 const fs = require('fs');
-let input = fs.readFileSync('./input.txt').toString().trim().split('\n'); //readFileSync(0) : stdin
 
 // const readline = require('readline');
 // const rl = readline.createInterface(fs.createReadStream('./input.txt'));
-const range = new Array(Number(input[0])).fill(0).map((el, idx) => idx+1);
-const numbers = input.slice(1).map(Number);
 
-// const id = Symbol('id');
+let input = fs.readFileSync('./input.txt').toString().trim(); //readFileSync(0) : stdin
+const [N, K] = input.split(' ').map(Number);
 
-// const obj = {
-//     [id] : 1,
-//     name : 'lsh'
-// }
+const dp = [[], [1]];
 
-// const obj1 = Object.assign({}, obj);
-// const obj2 = {...obj}
-
-function seeThis (...arg) {
-    console.log(this, ...arg);
+function find(N) {
+    const upperArr = dp[N-1];
+    const result = [];
+    for(let i=0 ; i<upperArr.length+1 ; i++) {
+        if(i === 0) result[i] = upperArr[0];
+        else if(i === upperArr.length) result[i] = upperArr[upperArr.length-1];
+        else result[i] = upperArr[i-1] +  upperArr[i];
+    }
+    return result;
 }
 
-const bindObj = {
-    name : 'bindObj'
+for(let i=2 ; i<=N+1 ; i++) {
+    dp[i] = find(i);
 }
 
-const bindObj2 = {
-    name : 'bindObj2'
-}
-
-const bindSeeThis = seeThis.bind(bindObj, 1, 2, 3).bind(bindObj2, 4, 5);
-
-const obj1 = {
-    name : 'obj1',
-    // seeThis
-    bindSeeThis
-}
-
-const obj2 = {
-    name : 'obj2',
-    // seeThis
-    bindSeeThis
-}
-
-const arr = [() => { console.log(this)}];
-
-seeThis();
-// obj1.seeThis();
-// obj2.seeThis();
-
-bindSeeThis(10);
-obj1.bindSeeThis(11);
-obj2.bindSeeThis(12);
-arr[0]();
-
-// bindSeeThis.call({ name : `I'm new object`}, 13);
-
-// const objLiteral = {
-//     name : 'literal',
-//     seeThis : () => {
-//         console.log(this);
-//     }
-// }
-
-// class MakeObj {
-//     constructor(arg) {
-//         this.name = arg;
-//     }
-//     seeThis = () => {
-//         console.log(this);
-//     }
-// }
-
-// const objInstance = new MakeObj('class');
-
-// objLiteral.seeThis();
-// objInstance.seeThis();
-
-// const arr = new Array(8).fill(1);
-
-// arr.seeThis = function() {
-//     console.log(this);
-// }
-
-// arr.seeThis();
-
+console.log(dp[N+1][K] % 10007); 
 
--- 기억해둘것들/this/this.js
+++ 기억해둘것들/this/this.js
@@ -2,10 +2,12 @@
 // 1. 함수를 그냥 실행할 때 그 함수의 this는 전역객체(window - 브라우저, global - 노드)
 // 2. 함수가 객체의 메서드로서 실행될 때 this는 그 객체
 // 2'. Function.prototype.bind, Function.prototype.call, Function.prototype.apply 메서드는 어떤 객체의 메서드로 실행될 지 정해줄 수 있다.
+
+
 // 3. new 키워드로 실행될 때 this는 빈객체가 되고 return문이 없어도 this가 반환된다.(return문에 원시 타입을 넣으면 무시, 참조 타입을 넣으면 덮어쓴다, - 중요x)
 
 // 예외) 화살표 함수
-// 화살표 함수의 this는 선언된 렉시컬 환경의 this를 따라간다.
+// 4. 화살표 함수의 this는 선언된 렉시컬 환경의 this를 따라간다.
 const obj = {
     name : 'lsh',
     seeName : function() {
